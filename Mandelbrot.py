89752c4fb2fb16f87b3548398966259fd7bbefb9
Amish <myodamian@gmail.com>
2023-11-03 14:40:20 +0100

    Function added to create image of mandelbrot set

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def mandelbrot(c: complex, iterations: int):
@@ -10,18 +11,13 @@ def mandelbrot(c: complex, iterations: int):
     :return: Value after all iterations
     """
 
-    # Get max value
-    maxvalue = np.finfo(complex).max
-
-    # Take square root
-    maxvalue = np.sqrt(maxvalue)
     # Initial value for z
     z = c
 
     # Looping
     for _ in range(iterations):
-        # If overflowing
-        if abs(z) > maxvalue:
+        # Goes to infinity
+        if abs(z) > 2:
             return np.inf
         else:
             z = z ** 2 + c
@@ -29,7 +25,7 @@ def mandelbrot(c: complex, iterations: int):
     return z
 
 
-def inMandelbrotSet(c, iterations):
+def inMandelbrotSet(c: complex, iterations: int):
     """
     Checks if c is in the Mandelbrot set
 
@@ -43,4 +39,88 @@ def inMandelbrotSet(c, iterations):
 
     return val != np.inf
 
+
+def mandelbrotSet(low: complex, high: complex, N: int, iterations: int):
+    """
+
+    :param low: Lowest value for real and imaginary part of c
+    :param high: Highest value for real and imaginary part of c
+    :param N: Number of points between low and high on real and imaginary axis
+    :param iterations: Number of iterations
+    :return: N x N array with bool where true represents in set
+    """
+
+    # Get all points
+    x = np.linspace(low, high, N)
+
+    # Create mandelbrot set
+    mandel = []
+
+    # Looping through all c's
+    for a in x:
+        # Saving for changing b's
+        lst = []
+        for b in x:
+            # Getting c
+            c = a + b * 1j
+
+            # Add to list
+            lst.append(inMandelbrotSet(c, iterations))
+
+        # Add to mandelbrot set
+        mandel.append(lst)
+
+    return np.array(mandel)
+
+
+def showMandelbrot(low: complex, high: complex, N: int, iterations: int,
+                   color: list = None, background: list = None):
+    """
+    Function used to show the mandelbrot set based on input
+
+    :param low: Lowest value for real and imaginary part of c
+    :param high: Highest value for real and imaginary part of c
+    :param N: Number of points between low and high on real and imaginary axis
+    :param iterations: Number of iterations
+    :param color: Color to give to points that belong to mandelbrot set
+    :param background: Color of background (parts that do not belong to mandelbrot set)
+    :return: Void
+    """
+    # Get mandelbrot set
+    mandel = mandelbrotSet(low, high, N, iterations).T
+
+    # Set colors
+    colors = np.zeros((N, N, 3))
+
+    # Setting the color
+    if color is None:
+        color = np.array([255, 255, 255])
+    else:
+        color = np.array(color)
+
+    # Setting the background
+    if background is None:
+        background = np.array([0, 0, 0])
+    else:
+        background = np.array(background)
+
+    for i in range(len(mandel)):
+        for j in range(len(mandel)):
+            if mandel[i, j]:
+                colors[i, j] = color
+            else:
+                colors[i, j] = background
+
+    # Float to int
+    colors = colors.astype(int)
+
+    # Show image
+    plt.imshow(colors, origin='lower', extent=[low, high, low, high])
+
+    # Set axis labels
+    plt.xlabel('$Real$')
+    plt.ylabel('$Imaginary$')
+
+    # Show image
+    plt.show()
 
