9ca9650cf0f5e5a67378b1f9a6808babe415aa14
Terry-ZJ-Zhang <terry.zj.zhang@gmail.com>
2023-11-15 00:05:59 +0100

    modified Mandelbrot_h.py

--- Mandelbrot_h.py
+++ Mandelbrot_h.py
@@ -1,13 +1,12 @@
 import numpy as np
 
 
-def mandelbrot(c: complex, iterations: int):
+def whenLeaveMandelbrotSet(c: complex, iterations: int):
     """
     Compute the mandelbrot equation with iter number of iterations
 
     :param c: Constant value
     :param iterations: Number of iterations
-    :return: Value after all iterations
     :return: The interation number when abs(z) becomes larger than 2
     """
 
@@ -22,20 +21,4 @@ def mandelbrot(c: complex, iterations: int):
         else:
             z = z**2 + c
 
-    return z, -1
-
-
-def inMandelbrotSet(c: complex, iterations: int):
-    """
-    Checks if c is in the Mandelbrot set
-
-    :param c: Constant value
-    :param iterations: Number of iterations
-    :return: True if in set, otherwise false
-    :return: When abs(z) > 2
-    """
-
-    # Check value
-    val, j = mandelbrot(c, iterations)
-
-    return val != np.inf, j
+    return j + 1
